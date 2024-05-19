bb124ae3063ac253fbdc9091d6d139accb05c8a5
Meet Solanki <99175906+meets4@users.noreply.github.com>
2024-05-19 19:52:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const typedText = new Typed(".text", {
+        strings: ["React Developer","Web Designer", "Photographer", "Dendrophile"],
+        typeSpeed: 70,
+        backSpeed: 30,
+        backDelay: 1000,
+        loop: true
+    });
+
+    const menuToggle = document.getElementById('menuToggle');
+    const menubar = document.getElementById('menubar');
+    const container = document.querySelector('.main-container');
+
+    menuToggle.addEventListener('click', function(event) {
+        menubar.style.transform = 'translateX(0px)';
+        menubar.style.opacity = '1';
+        menubar.style.transition = '0.6s';
+        container.style.transition = '0.6s';
+        container.style.filter = 'blur(10px)'
+        event.stopPropagation(); // Prevent click from propagating to document
+    });
+
+    document.addEventListener('click', function(event) {
+        if (!menubar.contains(event.target) && !menuToggle.contains(event.target)) {
+            menubar.style.transform = 'translateX(-100%)'; // or however you want to hide the menubar
+            container.style.filter = 'blur(0px)'
+        }
+    });
+});
